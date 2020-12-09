7cc7ca5108d2c6a88bad5e33a229574054cdd076
caoyu <1127caoyu@gmail.com>
2020-12-09 18:14:26 +0800

    数据获取

--- src/page/login/Login.vue
+++ src/page/login/Login.vue
@@ -5,8 +5,8 @@
       <div class="title">
         <img class="title_img" src="static/images/login_title1.png">
         <h4 class="title_text">使用 Smartisan ID 登录官网</h4>
-        <input class="input" placeholder="请输入账号">
-        <input class="input" type="password" placeholder="请输入密码">
+        <input class="input" type="number" v-model="q" ref="account" placeholder="请输入账号">
+        <input class="input" type="password" ref="password" placeholder="请输入密码">
         <div class="msg_box">
           <p class="chek_box">
             <input class="btn_chekbox" type="checkbox" id="chek">
@@ -14,20 +14,20 @@
           </p>
           <div class="msg_box2">
             <label class="btn_text">短信验证码登录</label>
-            <label class="line_vertical" />
-            <label class="btn_text" >忘记密码?</label>
+            <label class="line_vertical"/>
+            <label class="btn_text">忘记密码?</label>
           </div>
 
 
         </div>
-        <button class="btn_submit">登录</button>
+        <button class="btn_submit" @click="login()">登录</button>
 
-        <label class="line_h" />
+        <label class="line_h"/>
         <div class="bottom_box">
           <label class="btn_chekbox_label">登录代表你已同意</label>
-          <label class="btn_text" >&#8194用户协议&#8194</label>
-          <label class="btn_chekbox_label" >和&#8194</label>
-          <label class="btn_text" >隐私政策</label>
+          <label class="btn_text">&#8194用户协议&#8194</label>
+          <label class="btn_chekbox_label">和&#8194</label>
+          <label class="btn_text">隐私政策</label>
         </div>
       </div>
     </div>
@@ -37,7 +37,13 @@
 
 <script>
 export default {
-  name: "Login"
+
+  methods: {
+    login(){
+      window.console.log(this.$refs.account.value)
+      window.console.log(this.$refs.password.value)
+    }
+  }
 }
 </script>
 
