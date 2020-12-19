12eeb06123004f1e087b09cd4ef893cf52d458d4
caoyu <1127caoyu@gmail.com>
2020-12-19 23:20:12 +0800

    轮播图 吸顶

--- src/page/home/Home.vue
+++ src/page/home/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="home_bg">
     <div class="header_topbar">
       <div class="topbar_logo"/>
 
@@ -20,23 +20,59 @@
         </ul>
       </div>
       <div class="topbar_r">
-        <div class="topbar_user" v-on:mouseenter="show_user = true" v-on:mouseleave="show_user = false">
-          <div class="topbar_user_img"/>
+          <el-popover
+            placement="top-start"
+            trigger="hover" >
+            <div class="user shadow_bg">
+
+            </div>
+            <el-button slot="reference" class="topbar_user_img"/>
+          </el-popover>
+          <el-popover
+            placement="top-start"
+            trigger="hover">
+              <div class="cart shadow_bg">
+                <div class="cart_img"/>
+                <div class="cart_tips1">购物车为空</div>
+                <div class="cart_tips2">您还没有选购任何商品，现在前往商城选购吧！</div>
+              </div>
+              <el-button slot="reference" class="topbar_cart_img"/>
+          </el-popover>
         </div>
-        <div class="topbar_cart" v-on:mouseenter="show_cart = true" v-on:mouseleave="show_cart = false">
-          <div class="topbar_cart_img"/>
-        </div>
-      </div>
+    </div>
+    <!--吸顶-->
+    <div class="sticky-container">
 
     </div>
-    <div class="cart shadow_bg" v-show="show_cart" v-on:mouseenter="show_cart = true"
-         v-on:mouseleave="show_cart = false">
-      <div class="cart_img"/>
-      <div class="cart_tips1">购物车为空</div>
-      <div class="cart_tips2">您还没有选购任何商品，现在前往商城选购吧！</div>
+    <div class="shop shop_bg">
+      <el-carousel height="553px" autoplay = "true" interval = "5000">
+        <el-carousel-item v-for="(list,index) in bannerlist" :key="index">
+          <img :src=list.image>
+        </el-carousel-item>
+      </el-carousel>
     </div>
-    <div class="user shadow_bg" v-show="show_user" v-on:mouseenter="show_user = true"
-         v-on:mouseleave="show_user = false"/>
+
+    <div class="shop shop_bg">
+      <el-carousel height="553px" autoplay = "true" interval = "5000">
+        <el-carousel-item v-for="(list,index) in bannerlist" :key="index">
+          <img :src=list.image>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <div class="shop shop_bg">
+      <el-carousel height="553px" autoplay = "true" interval = "5000">
+        <el-carousel-item v-for="(list,index) in bannerlist" :key="index">
+          <img :src=list.image>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <div class="shop shop_bg"/>
+    <div class="shop shop_bg"/>
+    <div class="shop shop_bg"/>
+    <div class="shop shop_bg"/>
+    <div class="shop shop_bg"/>
+
+
   </div>
 
 
@@ -47,8 +83,13 @@ export default {
   name: "Home",
   data() {
     return {
-      show_cart: false,//购物车
-      show_user: false//用户
+      bannerlist:[
+        {image:"https://resource.smartisan.com/resource/3a6ac6d9102ffa499fd10b9f0ceeed77.png?x-oss-process=image/resize,w_1220/format,webp/quality,Q_95"},
+        {image:"https://resource.smartisan.com/resource/919344c17d614ac64e497859e062e988.png?x-oss-process=image/resize,w_1220/format,webp/quality,Q_95"},
+        {image:"https://resource.smartisan.com/resource/bedcb8b9cc461421de8bf1f186d2f150.jpg?x-oss-process=image/resize,w_1220/format,webp/quality,Q_95"},
+        {image:"https://resource.smartisan.com/resource/60c1fea58f4116837a644e101fdb7116.png?x-oss-process=image/resize,w_1220/format,webp/quality,Q_95"},
+        {image:"https://resource.smartisan.com/resource/d5f455111845c9542f399fa398c451f9.png?x-oss-process=image/resize,w_1220/format,webp/quality,Q_95"}
+      ]
     }
   }
 }
