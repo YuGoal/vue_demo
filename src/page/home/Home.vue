885b3b004978b8af93592f37ac90f097998c5b83
caoyu <1127caoyu@gmail.com>
2020-12-25 23:17:58 +0800

    首页整体布局

--- src/page/home/Home.vue
+++ src/page/home/Home.vue
@@ -9,27 +9,93 @@
       </el-carousel>
     </div>
 
-    <div class="shop shop_bg">
-      <el-carousel height="553px" autoplay="true" interval="5000">
-        <el-carousel-item v-for="(list,index) in bannerlist" :key="index">
-          <img :src=list.image>
-        </el-carousel-item>
-      </el-carousel>
+
+    <div class="activities shop_bg">
+      <div class="advertise">
+        <img class="advertise_img"
+          src="https://resource.smartisan.com/resource/fbefae00ef98859f342e9f56889899a0.jpg?x-oss-process=image/resize,w_600/format,webp">
+      </div>
+      <div class="advertise">
+        <img class="advertise_img"
+          src="https://resource.smartisan.com/resource/1023fe9691eaf03abbfd261b36df4985.png?x-oss-process=image/resize,w_600/format,webp">
+      </div>
+      <div class="advertise">
+        <img class="advertise_img"
+          src="https://resource.smartisan.com/resource/33ea4bfa05068e0741b7278fec9da8a6.jpg?x-oss-process=image/resize,w_600/format,webp">
+      </div>
+      <div class="advertise">
+        <img class="advertise_img"
+          src="https://resource.smartisan.com/resource/fdc4370d1ce14a67fadc35d74209ac0f.jpg?x-oss-process=image/resize,w_600/format,webp">
+      </div>
     </div>
-    <div class="shop shop_bg">
-      <el-carousel height="553px" autoplay="true" interval="5000">
-        <el-carousel-item v-for="(list,index) in bannerlist" :key="index">
-          <img :src=list.image>
-        </el-carousel-item>
-      </el-carousel>
+    <div class="common-normal-box shop_bg">
+      <div class="common-normal-header">
+        <h5 class="header_font">热门商品</h5>
+        <div class="right-operation">
+          <button class="btn_left"/>
+          <button class="btn_right"/>
+        </div>
+      </div>
+      <div  class="common-normal-body">
+
+      </div>
     </div>
-    <div class="shop shop_bg"/>
-    <div class="shop shop_bg"/>
-    <div class="shop shop_bg"/>
-    <div class="shop shop_bg"/>
-    <div class="shop shop_bg"/>
+    <div class="common-normal1-box shop_bg">
+      <div class="common-normal-header">
+        <h5 class="header_font">坚果 R2 及配件</h5>
+      </div>
+    </div>
+
+    <div class="common-normal1-box shop_bg">
+      <div class="common-normal-header">
+        <h5 class="header_font">Smartisan TNT</h5>
+      </div>
+    </div>
+
+    <div class="common-normal1-box shop_bg">
+      <div class="common-normal-header">
+        <h5 class="header_font">官方精选配件</h5>
+      </div>
+    </div>
 
 
+    <div class="common-normal1-box shop_bg">
+      <div class="common-normal-header">
+        <h5 class="header_font">服饰专场</h5>
+      </div>
+    </div>
+
+    <div class="common-normal1-box shop_bg">
+      <div class="common-normal-header">
+        <h5 class="header_font">抖音文创</h5>
+      </div>
+    </div>
+
+    <div class="common-normal1-box shop_bg">
+      <div class="common-normal-header">
+        <h5 class="header_font">箱包</h5>
+      </div>
+    </div>
+
+    <div class="common-normal1-box shop_bg">
+      <div class="common-normal-header">
+        <h5 class="header_font">足迹系列保护套</h5>
+      </div>
+    </div>
+
+    <div class="common-normal1-box shop_bg">
+      <div class="common-normal-header">
+        <h5 class="header_font">畅呼吸系列产品</h5>
+      </div>
+    </div>
+    <div class="common-normal2-box shop_bg">
+      <div class="common-normal-header">
+        <h5 class="header_font">论坛精选</h5>
+      </div>
+    </div>
+    <div class="footer">
+
+    </div>
   </div>
 
 
