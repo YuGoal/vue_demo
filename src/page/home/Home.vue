61e03ca209baf994b12dc26d46ff65b786fe1aab
caoyu <1127caoyu@gmail.com>
2021-01-03 11:02:45 +0800

    轮播图数据修改

--- src/page/home/Home.vue
+++ src/page/home/Home.vue
@@ -3,8 +3,8 @@
     <sm/>
     <div class="shop shop_bg">
       <el-carousel height="553px" autoplay="true" interval="5000">
-        <el-carousel-item v-for="(list,index) in bannerlist" :key="index">
-          <img :src=list.image>
+        <el-carousel-item v-for="(list,index) in home_carousel" :key="index">
+          <img class="carousel-img" :src=list.image[0]>
         </el-carousel-item>
       </el-carousel>
     </div>
@@ -117,13 +117,6 @@ export default {
   name: "Home",
   data() {
     return {
-      bannerlist: [
-        {image: "https://resource.smartisan.com/resource/3a6ac6d9102ffa499fd10b9f0ceeed77.png?x-oss-process=image/resize,w_1220/format,webp/quality,Q_95"},
-        {image: "https://resource.smartisan.com/resource/919344c17d614ac64e497859e062e988.png?x-oss-process=image/resize,w_1220/format,webp/quality,Q_95"},
-        {image: "https://resource.smartisan.com/resource/bedcb8b9cc461421de8bf1f186d2f150.jpg?x-oss-process=image/resize,w_1220/format,webp/quality,Q_95"},
-        {image: "https://resource.smartisan.com/resource/60c1fea58f4116837a644e101fdb7116.png?x-oss-process=image/resize,w_1220/format,webp/quality,Q_95"},
-        {image: "https://resource.smartisan.com/resource/d5f455111845c9542f399fa398c451f9.png?x-oss-process=image/resize,w_1220/format,webp/quality,Q_95"}
-      ],
       home_activities: [],
       home_carousel: [],
       home_dynamic: [],
